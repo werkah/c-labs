e5e435175d80021f4ef077df0a2d8aa41375728f
Weronika Hilaszek <92488733+werkah@users.noreply.github.com>
2023-04-25 19:59:17 +0200

    Update odd.c

--- odd.c
+++ odd.c
@@ -4,13 +4,10 @@ typedef unsigned long long num;
 
 num odddivisor(int n)
 {
-    //war koncowy rekursji
     if(n==1)
         return 1;
-    //dla nieparzystych
     if(n%2==1)
         return n+odddivisor(n-1);
-        //dla parzystych
     else
         return odddivisor(n/2)+n*n/4;
 }
